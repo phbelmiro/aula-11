c99c09c097a8ee3504eb954902b5aaa1febe52ce
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-09-01 21:49:46 -0300

    Create aula-11.js

---
+++ aula-11.js
@@ -0,0 +1,44 @@
+/*function imprimirOlaMundo() {
+//     console.log("ola mundo!");    
+// }
+
+// imprimirOlaMundo();
+
+// function name(nome) {
+//     console.log(`ola ${nome}`);
+// }
+
+// name(prompt("qual e seu nome?"));
+// name(prompt("qual e seu nome?"));
+// name(prompt("qual e seu nome?"));
+
+// const a = 1 
+
+// function imprimirVariavel() {
+//     const b = 2
+//     console.log('variavel a' , a)
+//     console.log('varivel b' , b)
+// }
+
+// imprimirVariavel()
+
+//     console.log('variavel a' , a)
+//     console.log('varivel b' , b)
+
+// function calculaArea(altura, largura) {
+//     const area = altura * largura
+//     return area
+// }
+
+// const areaCalculada = calculaArea(2, 3) 
+
+// console.log(calculaArea(2, 3))
+
+// function numeros(n1, n2) {
+//     const soma = n1 + n2
+//     return soma
+// }
+
+// const number = numeros (9, 1)
+// console.log(numeros(9, 1)
+*/
